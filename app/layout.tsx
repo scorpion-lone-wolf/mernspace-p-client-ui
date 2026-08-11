01c9a461234f7198b27f5905510ec75bf1d80738
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-08-11 18:00:02 +0530

    add font for put project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,10 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={cn("h-full", "antialiased", "font-manrope", manrope.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
