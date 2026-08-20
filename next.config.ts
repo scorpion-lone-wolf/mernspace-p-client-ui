5ed5b0162e317d9c0fde00943db7f27ba78f425d
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-08-20 23:29:51 +0530

    fetch product from server and display on respective category tab

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mernspace-p-project.s3.us-east-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
