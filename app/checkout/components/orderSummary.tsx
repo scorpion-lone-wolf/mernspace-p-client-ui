f592c642033732dcc0beb23bcc11df5dd1df703a
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-09-02 15:44:19 +0530

    add dynamic order price calculation

--- app/checkout/components/orderSummary.tsx
+++ app/checkout/components/orderSummary.tsx
@@ -2,11 +2,14 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { useAppSelector } from "@/lib/store/hooks";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
+const TAXES_PERCENTAGE = 0.12;
+const DELIVERY_CHARGES = 100;
 function OrderSummary() {
+  const [discountPercentage, setDiscountPercentage] = useState(0);
   const cart = useAppSelector(state => state.cart.cartItems);
-
+  //   subtotal
   const calculatedTotal = useMemo(() => {
     let total = 0;
     cart.forEach(item => {
@@ -26,7 +29,20 @@ function OrderSummary() {
 
     return total;
   }, [cart]);
-
+  // discount
+  const discountAmount = useMemo(() => {
+    return Math.round(calculatedTotal * (discountPercentage / 100));
+  }, [calculatedTotal, discountPercentage]);
+  //   taxes
+  const taxes = useMemo(() => {
+    const amountAfterDiscount = calculatedTotal - discountAmount;
+    // 12% tax (Currently hardcoded)
+    return Math.round(amountAfterDiscount * 0.12);
+  }, [calculatedTotal, discountAmount]);
+  //   grand total
+  const grandTotal = useMemo(() => {
+    return calculatedTotal - discountAmount + taxes + DELIVERY_CHARGES;
+  }, [calculatedTotal, taxes, discountAmount]);
   return (
     <Card className="w-2/5 border-none h-auto self-start">
       <CardHeader>
@@ -39,20 +55,20 @@ function OrderSummary() {
         </div>
         <div className="flex items-center justify-between">
           <span>Taxes</span>
-          <span className="font-bold">₹82</span>
+          <span className="font-bold">₹{taxes}</span>
         </div>
         <div className="flex items-center justify-between">
           <span>Delivery charges</span>
-          <span className="font-bold">₹100</span>
+          <span className="font-bold">₹{DELIVERY_CHARGES}</span>
         </div>
         <div className="flex items-center justify-between">
           <span>Discount</span>
-          <span className="font-bold">₹0</span>
+          <span className="font-bold">₹{discountAmount}</span>
         </div>
         <hr />
         <div className="flex items-center justify-between">
           <span className="font-bold">Order total</span>
-          <span className="font-bold">₹8300</span>
+          <span className="font-bold">₹{grandTotal}</span>
         </div>
         <div className="flex items-center gap-4">
           <Input id="fname" type="text" className="w-full" placeholder="Coupon code" />
